d7f4e0521e850fe56f752d6d0b8c9d668f1920a2
amurad77 <amurad7799@gmail.com>
2022-02-22 23:10:38 -0800

    fucking Mr Bullet

---
+++ Assets/Scripts/Main Manu Scripts/Level.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+
+public class Level : MonoBehaviour
+{
+    private Button levelBtn;
+
+    public int levelReq;
+
+
+    void Start()
+    {
+        levelBtn = GetComponent<Button>();
+
+        if(PlayerPrefs.GetInt("Level", 1) >= levelReq)
+            levelBtn.onClick.AddListener(() => LoadLevel());
+        else
+            GetComponent<CanvasGroup>().alpha = .5f;
+
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(gameObject.name);
+    }
+}
